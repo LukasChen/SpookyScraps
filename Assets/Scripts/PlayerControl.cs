a38ffd5b54845a362a9ab642de430ae10484fcda
LukasChen <villagelukaschen@gmail.com>
2024-01-22 23:38:59 +0800

    update with synty characters

--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -6,12 +6,15 @@ using UnityEngine.InputSystem;
 
 public class PlayerControl : MonoBehaviour {
     [SerializeField] private float _speed;
+    [SerializeField] private float _turnSmoothTime;
     
     
     private PlayerInputActions _inputs;
     private CharacterController _controller;
 
     private InputAction _movementAction;
+
+    private float _turnSmoothVelocity;
 
     private void OnEnable() {
        _inputs = new PlayerInputActions();
@@ -30,5 +33,12 @@ public class PlayerControl : MonoBehaviour {
         Vector3 movementInput = _movementAction.ReadValue<Vector2>();
         Vector3 movement = new Vector3(movementInput.x, 0, movementInput.y);
         _controller.Move(movement * (_speed * Time.deltaTime));
+
+        if (movement.magnitude >= 0.1f) {
+            Quaternion lookRotation = Quaternion.LookRotation(movement);
+            float lookAngle = lookRotation.eulerAngles.y;
+            float angle = Mathf.SmoothDamp(transform.eulerAngles.y, lookAngle, ref _turnSmoothVelocity, _turnSmoothTime);
+            transform.rotation = Quaternion.Euler(0, angle, 0);
+        }
     }
 }
